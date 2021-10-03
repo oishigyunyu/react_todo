f60935c541afe7f54a4e8853335cdb67abefab48
oishigyunyu <yamamuuuura@gmail.com>
2021-10-03 21:24:37 +0900

    added todos type

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,14 +1,40 @@
+// React から useState フックをインポート
+import { useState } from 'react';
+
+type Todo = {
+    value: string;
+};
+
 export const App = () => {
-    return (
-      <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="text" value="" onChange={(e) => e.preventDefault()} />
-          <input
-            type="submit"
-            value="追加"
-            onSubmit={(e) => e.preventDefault()}
-          />
-        </form>
-      </div>
-    );
-  };
+  /**
+   * text = ステートの値
+   * setText = ステートの値を更新するメソッド
+   */
+  const [text, setText] = useState('');
+  
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  return (
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        {/*
+          入力中テキストの値を text ステートが
+          持っているのでそれを value として表示
+
+          onChange イベント（＝入力テキストの変化）を
+          text ステートに反映する
+         */}
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="追加"
+          onSubmit={(e) => e.preventDefault()}
+        />
+      </form>
+    </div>
+  );
+};
