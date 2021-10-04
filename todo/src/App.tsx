3196d6d7a140113e70e84041906adbbe2d00f61b
oishigyunyu <yamamuuuura@gmail.com>
2021-10-04 16:57:49 +0900

    keyのwarningを解決する

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,47 +1,33 @@
-// React から useState フックをインポート
 import React, { useState } from 'react';
 
 type Todo = {
   value: string;
+  id: number;
 };
 
 
 
 export const App = () => {
-  /**
-   * text = ステートの値
-   * setText = ステートの値を更新するメソッド
-   */
   const [text, setText] = useState('');
-
   const [todos, setTodos] = useState<Todo[]>([]);
-
-  const handleOnSubmit = () => {
-    // 何も入力されていなかったらリターン
-    if (!text) return;
-
-    // 新しい Todo を作成
-    const newTodo: Todo = {
-      value: text,
-    };
-
-    /**
-     * スプレッド構文を用いて todos ステートのコピーへ newTodo を追加する
-     *
-     * 以下と同義
-     * const oldTodos = todos.slice();
-     * oldTodos.splice(0, 0, newTodo);
-     * setTodos(oldTodos);
-     *
-     **/
-    setTodos([newTodo, ...todos]);
-    // フォームへの入力をクリアする
-    setText('');
-  };
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
+
+  const handleOnSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+    setText('');
+  };
+
+  
 
   return (
     <div>
@@ -51,22 +37,14 @@ export const App = () => {
             handleOnSubmit();
       }}
       >
-        {/*
-          入力中テキストの値を text ステートが
-          持っているのでそれを value として表示
-
-          onChange イベント（＝入力テキストの変化）を
-          text ステートに反映する
-         */}
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => handleOnChange(e)}
-        />
-        <input
-          type="submit" value="追加" onSubmit={handleOnSubmit}
-        />
+        <input type="text" value={text} onChange={(e) => handleOnChange(e)} />
+        <input type="submit" value="追加" onSubmit={handleOnSubmit} />
       </form>
+      <ul>
+        {todos.map((todo) => {
+          return <li key={todo.id}>{todo.value}</li>;
+        })}
+      </ul>
     </div>
   );
 };
