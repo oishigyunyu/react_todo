90989cacf5d933703ffab882ba6f14a2bb0e0a9a
oishigyunyu <yamamuuuura@gmail.com>
2021-10-04 17:02:58 +0900

    追加したtodoを変更できるようにする

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -26,8 +26,17 @@ export const App = () => {
     setTodos([newTodo, ...todos]);
     setText('');
   };
-
   
+  const handleOnEdit = (id: number, value: string) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.value = value;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
 
   return (
     <div>
@@ -42,7 +51,14 @@ export const App = () => {
       </form>
       <ul>
         {todos.map((todo) => {
-          return <li key={todo.id}>{todo.value}</li>;
+          return (
+            <li key={todo.id}>
+              <input type="text"
+                     value={todo.value}
+                     onChange={(e) => handleOnEdit(todo.id, e.target.value)}
+              />
+            </li>
+          );
         })}
       </ul>
     </div>
